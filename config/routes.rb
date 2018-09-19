3cc452f3eb07b2d61e30253e016e0d3e8697c1e2
hirocueki <hirocueki@gmail.com>
2018-09-19 14:20:11 +0900

    ホームページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
-  get 'users/index'
+  root 'homes#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:index, :show] do
+  end
 end
