326a8c8016b957873f6205eef11aba7ce08b879a
hirocueki <hirocueki@gmail.com>
2018-09-20 17:56:58 +0900

    タグ取得時のN+1クエリを解消

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def index
     if user_signed_in?
-      @items = current_user.items.page(params[:page])
+      @items = current_user.items.includes(:tags).page(params[:page])
     end
   end
 end
