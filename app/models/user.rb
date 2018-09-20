76f405f9d91b42b91da4b89421249ec8b283a5fe
hirocueki <hirocueki@gmail.com>
2018-09-20 16:37:05 +0900

    UserとRelationshipにリレーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
 end
