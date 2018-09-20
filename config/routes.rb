da7cb844a05d0d45145053fc4673eee46db8fea4
hirocueki <hirocueki@gmail.com>
2018-09-20 17:04:25 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:index, :show] do
+    member do
+      get :following, :followers
+    end
     resources :items, except: [:index] do
       resources :comments
     end
